9d6872191b53c3102b945a49995f3227f668fe20
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-04-02 22:18:47 -0400

    _

--- haskell/programming_in_haskell/declaring_types_and_classes/type_declarations.hs
+++ haskell/programming_in_haskell/declaring_types_and_classes/type_declarations.hs
@@ -1,5 +1,14 @@
 -- Type declarations
 
+-- The simplest way to declare a a new type is to introduce a new name
+-- for an existing type. Note: types must begin with a Capital letter
+--
+-- 	type [A-Z].* = .*
+
 type String = [Char]
+
+-- Additionally, types may be declared in a nested fashion which can be
+-- seen as follows. 
+
 type Pos = (Int, Int)
 type Trans = Pos -> Pos
