83139165335eb1b65105a06b32ed8e26b5ac44e2
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-06-13 01:40:06 -0400

    update with type signature

--- haskell/euler/020.hs
+++ haskell/euler/020.hs
@@ -1,7 +1,8 @@
 import           Data.List
 
-ans = sum $ unfoldr (app) $ foldr (*) 1 [1..100]
-        where
-          app x = if x >= 1
-                     then Just (rem x 10, div x 10)
-                     else Nothing
+ans :: (Integral a, Foldable t) => t a -> a
+ans xs = sum $ unfoldr (app) $ foldr (*) 1 xs
+           where
+             app x = if x >= 1
+                        then Just (rem x 10, div x 10)
+                        else Nothing
