64c83ea237fbd209cf8942a3050b1161838408a3
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-07-30 00:53:52 -0400

    explore threading in python

--- python/threading/example.py
+++ python/threading/example.py
@@ -1,16 +1,16 @@
-from multiprocessing import Process
+from threading import Thread
 import time; import os;
 
 def function(sec):
+    print("Enter: threaded function")
     time.sleep(sec)
+    print("Exit : threaded function")
 
 if __name__ == "__main__":
-    info("main line")
     start = time.perf_counter()
 
-    processes = [Process(target = function, args = [6]) for _ in range(100)]
+    processes = [Thread(target = function, args = [6]) for _ in range(100)]
     starts = [process.start() for process in processes]
-
 
     ## Wait @ this line for process to complete.
     joins = [process.join() for process in processes]
