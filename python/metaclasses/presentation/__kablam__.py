6872b13fa752dd4502ae83ab01a91ab2093e9f67
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-10 18:57:17 -0400

    typechecking metaclass constructed

--- python/metaclasses/presentation/__kablam__.py
+++ python/metaclasses/presentation/__kablam__.py
@@ -10,28 +10,62 @@ class Meta(type):
             return fn(*args, **kwargs)
         return wrapper
 
+    @classmethod
+    def type_check(cls, fn):
+        def wrapper(*rgs, **kwargs):
+            annotations = fn.__annotations__
+            __self__, *__vars__ = fn.__code__.co_varnames
+            __self__, *__args__ = rgs
+            __kwargs__ = kwargs
+
+            __default__ = dict(zip(__vars__[len(fn.__defaults__):], fn.__defaults__))
+
+            __compiled_args__ = {}
+            for key, _ in annotations.items():
+                __compiled_args__[key] = __kwargs__[key] if key in __kwargs__.keys() else None
+
+            for key, value in __compiled_args__.items():
+                if value == None and len(__args__) > 0:
+                    __compiled_args__[key] = __args__.pop(0)
+
+            for key, value in __compiled_args__.items():
+                if value == None and key != 'return':
+                    __compiled_args__[key] = __default__[key]
+
+            for var in __vars__:
+                if var not in annotations.keys():
+                    raise TypeError(f"parameter {var} must be strictly typed")
+                elif isinstance(__compiled_args__[var], annotations[var]):
+                    continue
+                else:
+                    raise TypeError(\
+                            f"""parameter {var} = {__compiled_args__[var]} must be type {annotations[var]}, currently {type(__compiled_args__[var])}""")
+
+            retval = fn(*rgs, **kwargs)
+
+            if not isinstance(retval, annotations['return']):
+                raise TypeError(\
+                        f"""retval = {retval} must be type {annotations[var]}, currently {type(retval)}""")
+
+            return retval
+        return wrapper
+
+
     def __prepare__(name, bases, **kwargs):
-        print('Meta.__prepare__')
-        import pdb;pdb.set_trace()
-        return {"FUNCTION": lambda s, x: print(s, x) }
+        return {}
 
     def __new__(cls, name, bases, attrs, **kwargs):
-        import pdb;pdb.set_trace()
         for name, value in attrs.items():
-
             if isinstance(value, (types.FunctionType, types.MethodType)):
-
-                if (name in attrs['__greeting__']):
-                    attrs[name] = Meta.greeting(value)
+                if (name in attrs['__type_check__']):
+                    attrs[name] = Meta.type_check(value)
 
         return super().__new__(cls, name, bases, attrs)
 
-    ## def __init__(cls, name, bases, attrs, **kwargs):
-    ##     print('Meta.__init__')
-    ##     return super().__init__(name, bases, attrs)
+    def __init__(cls, name, bases, attrs, **kwargs):
+        return super().__init__(name, bases, attrs)
 
     def __call__(cls, *args, **kwargs):
-        print('Meta.__call__')
         for elmt in args:
             if not isinstance(elmt, (float, int)):
                 raise TypeError(f'Argument {elmt} must be a int or float.')
--- python/metaclasses/presentation/rectangle.py
+++ python/metaclasses/presentation/rectangle.py
@@ -2,21 +2,24 @@ from __kablam__ import Meta
 
 def A():
     class rectangle(object, metaclass = Meta, this = 1, that = 2):
-        __greeting__ = ['area', 'perimiter']
+        __type_check__ = ['area', 'perimiter', 'function_']
 
-        def __init__(self, width, height):
-            print('rectangle.__init__')
-            self._width = width
-            self._height = height
+        def __init__(self:'rectangle', width:int = 10, height:int = 10):
+            self._width : int = width
+            self._height : int = height
 
-        def perimiter(self):
+        def perimiter(self) -> int:
             return 2*(self._width + self._height)
 
-        def area(self):
+        def area(self) -> int:
             return self._width*self._height
 
-        def __str__(self):
+        def __str__(self) -> str:
             return f"Rectangle: H - {self._height}, W - {self._width}"
+
+        def function_(self, a:int, b:int, c:int, d:int = 10, e:int = 20, f:int = 30) -> int:
+            return a + b + c + d + e + f
+
     return rectangle
 
 if __name__ == "__main__":
