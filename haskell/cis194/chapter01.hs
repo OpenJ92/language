1af0e6299b0afa7a992925ad14f0f05d9d08b782
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-16 13:18:19 -0400

    cis194 PS1 sec 1 completed

---
+++ haskell/cis194/chapter01.hs
@@ -0,0 +1,30 @@
+-- Problem Set 1: https://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
+
+-- problem 1
+toDigits :: Int -> [Int]
+toDigits = reverse . getDigits
+
+getDigits :: Int -> [Int]
+getDigits n | n > 10 = (mod n 10) : getDigits (div n 10)
+            | n >= 0 = [n]
+            | otherwise = []
+
+-- problem 2 --> apply to reversed list constructed in getDigits
+doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther = doubleEveryOther' False
+
+doubleEveryOther' :: Bool -> [Int] -> [Int]
+doubleEveryOther' p [    ] = []
+doubleEveryOther' p (x:xs) | p = (*) 2 x : doubleEveryOther' (not p) xs
+                           | otherwise = x : doubleEveryOther' (not p) xs
+
+-- problem 3
+sumDigits :: [Int] -> Int
+sumDigits [x] = x
+sumDigits (x:xs) = (sumDigits digits) + sumDigits xs
+                   where
+                     digits = getDigits x
+
+-- problem 4
+validate :: Int -> Bool
+validate = (==0) . (flip rem 10) . sumDigits . doubleEveryOther . getDigits 
