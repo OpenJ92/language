54ede4c50393f1dfeeab32d1d6c1f8081fa52a56
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-18 18:48:48 -0400

    Complete p1 and 2

--- haskell/cis194/chapter03.hs
+++ haskell/cis194/chapter03.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+ import Data.Char
+
  seive :: [a] -> Int -> [a]
  seive xs n = res
    where 
@@ -8,3 +10,25 @@ module Golf where
  -- problem 1
  skips :: [a] -> [[a]]
  skips xs = zipWith (seive) (replicate (length xs) xs) ([1..])
+
+ -- problem 2 -- continue tomorrow.
+ -- localMaxima (x:y:z:[]) = []
+ -- localMaxima (x:y:) = []
+
+ extrude :: (Ord a, Num a) => [a] -> [[Bool]]
+ extrude values  
+   | all (0>=) values = []
+   | otherwise = skim : extrude next
+   where
+     skim = map (0>=) values
+     next = map (flip (-) 1) values
+
+ -- problem 3
+ histogram :: (Ord a , Num a) => [a] -> [Char]
+ histogram values = unlines (reverse (footer ++ map (map (convert)) (extrude values)))
+   where
+     convert True  = ' '
+     convert False = '*'
+     footer = [map (intToDigit) numbers, border]
+     numbers = [1..(length values)]
+     border = replicate (length values) '='
