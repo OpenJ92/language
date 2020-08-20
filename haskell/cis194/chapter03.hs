b04706509daf7e26dc5a51e7eb001234236f435c
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-20 10:39:30 -0400

    continued work on chapter 3

--- haskell/cis194/chapter03.hs
+++ haskell/cis194/chapter03.hs
@@ -1,34 +1,34 @@
 module Golf where
 
- import Data.Char
+  import Data.Char
 
- seive :: [a] -> Int -> [a]
- seive xs n = res
-   where 
-     (_, res) = unzip (filter (\(x,y) -> (mod x n) == 0) (zip [1..] xs))
+  seive :: [a] -> Int -> [a]
+  seive xs n = res
+    where 
+      (_, res) = unzip (filter (\(x,y) -> (mod x n) == 0) (zip [1..] xs))
 
- -- problem 1
- skips :: [a] -> [[a]]
- skips xs = zipWith (seive) (replicate (length xs) xs) ([1..])
+  -- problem 1
+  skips :: [a] -> [[a]]
+  skips xs = zipWith (seive) (replicate (length xs) xs) ([1..])
 
- -- problem 2 -- continue tomorrow.
- -- localMaxima (x:y:z:[]) = []
- -- localMaxima (x:y:) = []
+  -- problem 2 -- continue tomorrow.
+  -- localMaxima (x:y:z:[]) = []
+  -- localMaxima (x:y:) = []
 
- extrude :: (Ord a, Num a) => [a] -> [[Bool]]
- extrude values  
-   | all (0>=) values = []
-   | otherwise = skim : extrude next
-   where
-     skim = map (0>=) values
-     next = map (flip (-) 1) values
+  extrude :: (Ord a, Num a) => [a] -> [[Bool]]
+  extrude values  
+    | all (0>=) values = []
+    | otherwise = skim : extrude next
+    where
+      skim = map (0>=) values
+      next = map (flip (-) 1) values
 
- -- problem 3
- histogram :: (Ord a , Num a) => [a] -> [Char]
- histogram values = unlines (reverse (footer ++ map (map (convert)) (extrude values)))
-   where
-     convert True  = ' '
-     convert False = '*'
-     footer = [map (intToDigit) numbers, border]
-     numbers = [1..(length values)]
-     border = replicate (length values) '='
+  -- problem 3
+  histogram' :: (Ord a , Num a) => [a] -> [Char]
+  histogram' values = unlines (reverse (footer ++ map (map (convert)) (extrude values)))
+    where
+      convert True  = ' '
+      convert False = '*'
+      footer = [map (intToDigit) numbers, border]
+      numbers = [1..(length values)]
+      border = replicate (length values) '='
