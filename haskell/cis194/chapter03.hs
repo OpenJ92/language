2275ad5483437f6e1eccca951982d378744a1d43
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-21 16:56:22 -0400

    complete chapter 3 haskell cis194 hw

--- haskell/cis194/chapter03.hs
+++ haskell/cis194/chapter03.hs
@@ -11,9 +11,20 @@ module Golf where
   skips :: [a] -> [[a]]
   skips xs = zipWith (seive) (replicate (length xs) xs) ([1..])
 
-  -- problem 2 -- continue tomorrow.
-  -- localMaxima (x:y:z:[]) = []
-  -- localMaxima (x:y:) = []
+  -- problem 2
+  localMaxima (x:y:z:zs)
+    | y > x && y > z = y : localMaxima (y:z:zs)
+    | otherwise      = localMaxima (y:z:zs)
+  localMaxima _ = []
+
+  partition :: (Eq a) => a -> [a] -> ([a], [a])
+  partition n xs = (filter (==n) xs, filter (/=n) xs)
+  
+  construct_histogram' :: (Num a, Eq a) => a -> [a] -> [(a, Int)]
+  construct_histogram' _ [] = []
+  construct_histogram' x xs = (x, length part) : construct_histogram' (x-1) ition
+    where
+      (part, ition) = partition x xs
 
   extrude :: (Ord a, Num a) => [a] -> [[Bool]]
   extrude values  
@@ -29,6 +40,9 @@ module Golf where
     where
       convert True  = ' '
       convert False = '*'
-      footer = [map (intToDigit) numbers, border]
-      numbers = [1..(length values)]
-      border = replicate (length values) '='
+      footer = [map (intToDigit) [1..(length values)], replicate (length values) '=']
+
+  histogram :: (Ord a, Num a) => [a] -> [Char]
+  histogram xs = histogram' $ map snd $ construct_histogram' m xs
+    where
+      m = maximum xs
