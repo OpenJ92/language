d1a5694685d8c55b26faf9d00ea83f908149c574
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-22 17:53:37 -0400

    remove template functions from p 1 and 2

--- haskell/cis194/chapter04.hs
+++ haskell/cis194/chapter04.hs
@@ -1,24 +1,12 @@
 module HOF where
   
   -- problem 1
-  fun1 :: [Integer] -> Integer
-  fun1 [] = 1
-  fun1 (x:xs)
-    | even x = (x - 2) * fun1 xs
-    | otherwise = fun1 xs
-  
   fun1' :: [Integer] -> Integer
   fun1' = foldr (*) 1 . map (flip (-) 2) . filter even
 
   -- problem 2 -- I'm not sure if this qualifies
   -- as wholemeal programming. Ask at the next 
   -- FP Study Group.
-  fun2 :: Integer -> Integer
-  fun2 1 = 0
-  fun2 n 
-    | even n = n + fun2 (n `div` 2)
-    | otherwise = fun2 (3 * n + 1)
-
   fun2' :: Integer -> Integer
   fun2' = sum . map (sum) . make_total
 
