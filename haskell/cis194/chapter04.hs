0f379811accd7cf7b2507b5661bbb0eb05a17e9d
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-22 17:39:03 -0400

    Start work on cis chapter 04

---
+++ haskell/cis194/chapter04.hs
@@ -0,0 +1,55 @@
+module HOF where
+  
+  -- problem 1
+  fun1 :: [Integer] -> Integer
+  fun1 [] = 1
+  fun1 (x:xs)
+    | even x = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+  
+  fun1' :: [Integer] -> Integer
+  fun1' = foldr (*) 1 . map (flip (-) 2) . filter even
+
+  -- problem 2 -- I'm not sure if this qualifies
+  -- as wholemeal programming. Ask at the next 
+  -- FP Study Group.
+  fun2 :: Integer -> Integer
+  fun2 1 = 0
+  fun2 n 
+    | even n = n + fun2 (n `div` 2)
+    | otherwise = fun2 (3 * n + 1)
+
+  fun2' :: Integer -> Integer
+  fun2' = sum . map (sum) . make_total
+
+  make_partial :: Integer -> [Integer]
+  make_partial = takeWhile (even) 
+        . iterate (flip div 2) 
+        . (\n -> if even n then n else 3 * n + 1)
+
+  make_total :: Integer -> [[Integer]]
+  make_total 1 = [] 
+  make_total n = partial : make_total continu
+   where
+     partial = make_partial n
+     continu = div (last partial) 2
+
+  -- problem 3
+  
+  -- problem 4
+  xor :: [Bool] -> Bool
+  xor = odd . length . filter (==True)
+
+  -- problem 5
+  map' :: (a -> b) -> [a] -> [b]
+  map' f = foldr ((:) . f) []
+
+  -- problem 6
+  myfoldr :: (a -> b -> a) -> a -> [b] -> a
+  myfoldr _ hook    [    ] = hook
+  myfoldr f hook (b:bs) = f (myfoldr f hook bs) (b)
+
+  myfoldl :: (b -> a -> a) -> a -> [b] -> a
+  myfoldl f hook (b:bs) = myfoldl f a bs
+    where
+      a = f b hook
