bd349511680eed50d6aae88470158776dda31f17
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-25 20:59:30 -0400

    remove unneeded list cast

--- python/leetcode/1380.py
+++ python/leetcode/1380.py
@@ -1,10 +1,10 @@
 class Solution:
-    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+    def luckyNumbers (self, matrix):
 
         m = len(matrix[0])
         n = len(matrix)
 
         rows = matrix
-        cols = list(map(list, list(zip(*matrix))))
+        cols = list(map(list,zip(*matrix)))
 
         return list(set(map(min, rows)).intersection(set(map(max, cols))))
