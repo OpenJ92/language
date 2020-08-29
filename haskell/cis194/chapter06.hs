e7b54b7ce961f31192ee8dc36d89dd6f5728e036
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-08-28 20:41:40 -0400

    complete required problems in 06

--- haskell/cis194/chapter06.hs
+++ haskell/cis194/chapter06.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Bits
 
 fib :: Integer -> Integer
 fib n 
@@ -15,7 +16,7 @@ fibs2 = unfoldr (\(f0, f1) -> Just (f1, (f1, f0+f1))) (0, 1)
 data Stream a = Element a (Stream a)
 
 instance (Show a) => Show (Stream a) where
-  show stream = show (take 20 (streamtolist stream))
+  show stream = show (take 200 (streamtolist stream))
 
 streamtolist :: Stream a -> [a]
 streamtolist (Element x xs) = x : streamtolist xs
@@ -24,7 +25,8 @@ streamRepeat :: a -> Stream a
 streamRepeat n = Element n (streamRepeat n)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap f (Element x elems) = Element (f x) (streamMap f elems)
+streamMap f (Element x elems) 
+  = Element (f x) (streamMap f elems)
 
 streamFromSeed :: a -> (a -> a) -> Stream a
 streamFromSeed n f = Element n (streamFromSeed (f n) f)
@@ -35,3 +37,10 @@ nats = streamFromSeed 1 (+1)
 interleaveStream :: a -> Stream a -> Stream a
 interleaveStream n (Element x elems) 
   = Element n (Element x (interleaveStream n elems))
+
+-- Reference: https://www.geeksforgeeks.org/highest-power-of-two-that-divides-a-given-number/
+ruler :: Stream Integer
+ruler 
+  = streamMap (f) . interleaveStream 0 $ streamFromSeed 2 (+2)
+  where
+    f n = (.&.) (n) (complement (n - 1))
