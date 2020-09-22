ed453f9c1f8ddb6ae67995cb6bc4557a73b13554
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-09-21 22:58:45 -0400

    problem 39 started.

---
+++ python/leetcode/0039.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        retval = []
+        for next_considered in candidates:
+            self.dfs(target, next_considered, candidates, [], retval)
+        return retval
+
+    def dfs(self, target, considered, candidates, subset, retval):
+        new_target = target - considered
+        import pdb;pdb.set_trace()
+
+        if (new_target < 0):
+            return
+        elif (new_target == 0):
+            subset.append(considered)
+            retval.append(subset)
+            return
+        else:
+            ## copy array into new set, pass new subset into the recursive call.
+            subset.append(considered)
+            for next_considered in candidates:
+                self.dfs(new_target, next_considered, candidates, subset, retval)
+
+        return retval
