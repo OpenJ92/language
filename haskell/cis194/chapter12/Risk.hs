af02a9fa04fb1ce16769e1b8a15dc83acd42d825
OpenJ92 <jacob.vartuli.92@gmail.com>
2020-09-24 14:49:44 -0400

    Start question 3

--- haskell/cis194/chapter12/Risk.hs
+++ haskell/cis194/chapter12/Risk.hs
@@ -15,7 +15,7 @@ first :: (a -> b) -> (a, c) -> (b, c)
 first f (a, c) = (f a, c)
 
 instance Random DieValue where
-  random           = first DV . randomR (1,6)
+  random           = first DV . randomR (1,6) 
   randomR (low,hi) = first DV . randomR (max 1 (unDV low), min 6 (unDV hi))
 
 die :: Rand StdGen DieValue
@@ -27,13 +27,55 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
-battlefield = Battlefield 10 12
+  deriving (Show)
+
+instance Num Battlefield where
+  (-) (Battlefield att def) (Battlefield att' def') = Battlefield (att - att') (def - def')
+  (*) _ _         = undefined
+  (+) _ _         = undefined
+  (fromInteger) _ = undefined
+  (signum) _      = undefined
+  (abs) _         = undefined
+
+
+battlefield = Battlefield 20 20
 
 rollDice :: Int -> Rand StdGen [DieValue]
 rollDice n = sequence $ replicate n die
 
-rollAttacker :: (Battlefield -> Army) -> Battlefield -> Rand StdGen [DieValue]
-rollAttacker role = (<$>) (reverse . sort . drop 1 . take 3) . rollDice . role
+policyAttackers :: Battlefield -> Int
+policyAttackers battlefield
+  | att > 3   = 3
+  | att > 0   = att - 1
+  | otherwise = 0
+  where att = attackers battlefield
+
+policyDefenders :: Battlefield -> Int
+policyDefenders battlefield
+  | def > 2   = 2
+  | def > 0   = def
+  | otherwise = 0
+  where def = defenders battlefield
+
+roll :: (Battlefield -> Int) -> Battlefield -> Rand StdGen [DieValue]
+roll policy = (<$>) (reverse . sort) . rollDice . policy
+
+fight :: Battlefield -> Rand StdGen [Bool]
+fight battlefield = zipWith (>) 
+                 <$> roll policyAttackers battlefield 
+                 <*> roll policyDefenders battlefield
+
+countlosses :: Rand StdGen [Bool] -> Rand StdGen Battlefield
+countlosses losses = Battlefield
+                  <$> ((length . filter (==True))  <$> losses)
+                  <*> ((length . filter (==False)) <$> losses)
 
 battle :: Battlefield -> Rand StdGen Battlefield
-battle = undefined
+battle battlefield = ((-) battlefield) <$> losses
+ where
+   losses = countlosses $ fight battlefield
+
+-- This is the infinite version of the process. How
+-- do we carry out checks for stopping resursion?
+invade :: Battlefield -> Rand StdGen Battlefield
+invade battlefield = battle battlefield >>= invade
