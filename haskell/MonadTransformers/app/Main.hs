15fcaac9d52fa223bb6e27e3cfa8594e007c20d3
OpenJ92 <jacob.vartuli.92@gmail.com>
2021-04-17 17:37:15 -0400

    Working with State Monads

---
+++ haskell/MonadTransformers/app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Lib
+import StateT
+import Control.Monad.State.Lazy
+
+main :: IO ()
+main = putStrLn "Hello World!"
---
+++ haskell/MonadTransformers/src/StateT.hs
@@ -0,0 +1,53 @@
+module StateT where
+
+import Control.Monad.State.Lazy
+
+type VitalForce = Int
+
+data Part 
+  = Leg
+  | Arm
+  | Torso
+  | Head
+  deriving (Show)
+
+data VitalityPart
+  = Live Part
+  | Dead Part
+  deriving (Show)
+
+data Body = Body VitalityPart VitalityPart VitalityPart VitalityPart
+  deriving (Show)
+
+makeBody = Body <$> f' (Dead Arm) <*> f' (Dead Torso) <*> f' (Live Head) <*> f' (Dead Leg)
+
+f :: VitalityPart -> State VitalForce VitalityPart
+f part = state $ \vf -> (makeAlive part, updateVitalForce part vf)
+
+f' :: VitalityPart -> StateT VitalForce Maybe VitalityPart
+f' part = StateT $ \vf -> 
+  if vf > 0 
+     then Just (makeAlive part, updateVitalForce part vf) 
+     else Nothing
+
+updateVitalForce :: VitalityPart -> (VitalForce -> VitalForce)
+updateVitalForce (Live _)     = id
+updateVitalForce (Dead Leg)   = \s -> s - 5
+updateVitalForce (Dead Arm)   = \s -> s - 4
+updateVitalForce (Dead Torso) = \s -> s - 10
+updateVitalForce (Dead Head)  = \s -> s - 15
+
+makeAlive :: VitalityPart -> VitalityPart
+makeAlive l@(Live _) = l
+makeAlive (Dead part) = Live part
+
+func :: (s -> s) -> (a -> b) -> a -> State s b 
+func sf f a = state f'
+  where
+    f' = \s -> (f a, sf s) 
+
+ex :: State Bool Int
+ex = pure 1 
+  >>= func (\s -> not s) ((+) 10) 
+  >>= func (\s -> not s) ((+) 12)
+  >>= func (\s -> not s) ((+) 14)
