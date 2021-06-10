5230e0694ea3a58f51d7153b8e833b8d8621a733
OpenJ92 <jacob.vartuli.92@gmail.com>
2021-06-10 14:44:09 -0400

    update conf

--- haskell/cis552/app/Main.hs
+++ haskell/cis552/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Lib
 import Test.HUnit
+import Text.ParserCombinators.Parsec
 
 -- Lectures
 import Lec1
@@ -16,9 +17,24 @@ import Kata
 import Classes
 import MonoidFoldable
 import QuickCheck
+import Persistent
 
 -- Homework
 import HW01
+import HW02
 
 main :: IO ()
-main = someFunc
+main = getLine >>= putStrLn
+
+g :: Monad m => (a -> m b) -> [a] -> m [b]
+g f [    ]  = return []
+g f (x:xs)  = f x >>= \x' -> g f xs >>= \xs' -> pure $ (:) x' xs'
+
+h :: Monad m => (a -> b -> m a) -> a -> [b] -> m a
+h _ acc [    ] = return acc
+h f acc (y:ys) = f acc y >>= \acc' -> h f acc' ys
+
+i' :: Monad m => [m a] -> m [a]
+i' [      ] = return []
+i' (mx:mxs) = mx >>= \x -> i' mxs >>= \xs -> pure $ (:) x xs
+
